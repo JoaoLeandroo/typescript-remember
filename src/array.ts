8daaa03ac46e2783b2a81c443694a3d278b986d0
joaoleandro <new.joaoleandro@gmail.com>
2024-02-06 15:56:54 -0300

    array remember

---
+++ src/array.ts
@@ -0,0 +1,18 @@
+const filmes: string[] = ["Harry Potter", "Rocky Balboa 1", "Rocky Balboa IV"];
+const numeros: number[] = [122, 99.2, 12312, 923323];
+
+const teste: boolean[] = [];
+teste.push(true);
+teste.push(false);
+console.log(teste);
+
+const doisTipos: Array<string | boolean> = []
+doisTipos.push("Testing...")
+doisTipos.push(true)
+console.log(doisTipos)
+
+const exemploTwo: (string | number)[] = []
+exemploTwo.push(52)
+exemploTwo.push("futebol")
+console.log(exemploTwo)
+
