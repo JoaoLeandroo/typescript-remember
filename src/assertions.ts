9a32534abd762b497bb6de181b03b861a9329e04
joaoleandro <new.joaoleandro@gmail.com>
2024-02-06 16:05:03 -0300

    assertions

---
+++ src/assertions.ts
@@ -0,0 +1,9 @@
+let statusAtual: unknown = 1;
+let mudaStatus: number = 0;
+mudaStatus = statusAtual as number;
+
+console.log(mudaStatus);
+
+let query: unknown = "Pizza";
+let search: string = query as string
+console.log(search)
