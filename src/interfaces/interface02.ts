0ddb3286e8cb3892b790fdb21b06b941d4909639
joaoleandro <new.joaoleandro@gmail.com>
2024-02-07 13:37:05 -0300

    opcional

--- src/interfaces/interface02.ts
+++ src/interfaces/interface02.ts
@@ -10,3 +10,9 @@ const newUser: CadastroProps = {
     email: "email@email.com",
     status: true
 }
+
+const testeUser = ({nome = "Não enviou o nome", email, status}: CadastroProps) => {
+    console.log(nome)
+}
+
+testeUser({ nome: "Ana", email: "teste", status: true })
