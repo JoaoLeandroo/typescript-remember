91f1eab0e8407765a2e7521c9da0ad5e55dae776
joaoleandro <new.joaoleandro@gmail.com>
2024-02-07 13:25:46 -0300

    interface optional channing

---
+++ src/interfaces/interface02.ts
@@ -0,0 +1,12 @@
+
+interface CadastroProps {
+    nome?: string;
+    email: string;
+    status: boolean;
+}
+
+const newUser: CadastroProps = {
+    // nome: "teste",
+    email: "email@email.com",
+    status: true
+}
